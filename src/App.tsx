878e84d6162dcf5345178a049fe1f0cbef443cdd
Jagruti345 <jp4496262@gmail.com>
2025-04-15 12:31:00 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skill'
+import Footer from './components/Footer'
 
 function App() {
  
